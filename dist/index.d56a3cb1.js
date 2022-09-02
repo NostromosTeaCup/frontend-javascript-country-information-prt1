4e4aafa1159f4d9ef5f1e031d8088e2c0c622de8
NostromosTeaCup <lvanmierlo@protonmail.com>
2022-09-02 15:41:49 +0200

    js gedaan, error opvangen nog oplossen

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -462,6 +462,57 @@ function hmrAcceptRun(bundle, id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
+// api binnenhalen via async functie
+//try / catch
+async function fetchCountryList() {
+    try {
+        const response = await _axiosDefault.default.get('https://restcountries.com/v2/all');
+        const countries = response.data;
+        //sorteren van landen op populatie
+        countries.sort((a, b)=>{
+            return a.population - b.population;
+        });
+        createCountryList(countries);
+    } catch (e) {
+        console.error(e);
+    }
+}
+//   //  errors communiceren in browser
+//     if (e.response.status === 500) {
+//         errorMessage.innerText = "Er ging iets mis met de server";
+//     } else if (e.response.status === 404) {
+//         errorMessage.innerText = "De pagina is niet gevonden";
+//     }
+// }
+fetchCountryList();
+function createCountryList(countries) {
+    //koppelen aan id in html file
+    const countryList = document.getElementById('countries-list');
+    countryList.innerHTML = countries.map((country)=>{
+        return `
+    <li>
+<img src="${country.flag}" alt="Flag of ${country.name}" class="flag"/>
+<span class="${continent(country.region)}">${country.name}</span>
+<p class="population">Has a population of ${country.population} people</p>
+</li>
+`;
+    }).join('');
+}
+//kleur geven aan continenten waar het land in ligt.
+function continent(current) {
+    switch(current){
+        case 'Africa':
+            return blue;
+        case 'Americas':
+            return green;
+        case 'Asia':
+            return red;
+        case 'Europe':
+            return yellow;
+        case 'Oceania':
+            return purple;
+    }
+}
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 module.exports = require('./lib/axios');
--- src/app.js
+++ src/app.js
@@ -1,1 +1,64 @@
 import axios from "axios";
+
+// api binnenhalen via async functie
+//try / catch
+
+async function fetchCountryList() {
+    try {
+        const response = await axios.get('https://restcountries.com/v2/all');
+        const countries = response.data;
+
+        //sorteren van landen op populatie
+        countries.sort((a, b) => {
+            return a.population - b.population;
+        });
+
+        createCountryList(countries);
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+    //   //  errors communiceren in browser
+    //     if (e.response.status === 500) {
+    //         errorMessage.innerText = "Er ging iets mis met de server";
+    //     } else if (e.response.status === 404) {
+    //         errorMessage.innerText = "De pagina is niet gevonden";
+    //     }
+    // }
+
+    fetchCountryList();
+
+    function createCountryList(countries) {
+        //koppelen aan id in html file
+        const countryList = document.getElementById('countries-list')
+
+
+        countryList.innerHTML = countries.map((country) => {
+            return `
+    <li>
+<img src="${country.flag}" alt="Flag of ${country.name}" class="flag"/>
+<span class="${continent(country.region)}">${country.name}</span>
+<p class="population">Has a population of ${country.population} people</p>
+</li>
+`;
+        }).join('');
+
+
+}
+//kleur geven aan continenten waar het land in ligt.
+function continent(current) {
+        switch (current) {
+            case 'Africa':
+                return blue;
+            case 'Americas':
+                return green;
+            case 'Asia':
+                return red;
+            case 'Europe':
+                return yellow;
+            case 'Oceania':
+                return purple;
+        }
+
+}
